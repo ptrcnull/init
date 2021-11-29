945b295bdf1851d915048bc87f0966f512fbdb26
ptrcnull <git@ptrcnull.me>
2021-11-29 22:12:52 +0100

    wip: Add opening /etc/inittab

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-  
+	inittab := DefaultInitTab
+	if file, err := os.OpenFile("/etc/inittab", os.O_RDONLY, 0644); err != nil {
+		inittab = ParseInitTab(file)
+		err := file.Close()
+		if err != nil {
+			fmt.Println("close inittab:", err)
+		}
+	}
+
 }
